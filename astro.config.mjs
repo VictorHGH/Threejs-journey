e354cda13c143aa284965a8976ac34795d4ce5b1
Victor Hugo González Hernández <64684570+Pikkirrin@users.noreply.github.com>
2022-12-10 22:23:32 -0600

    New changes to netlify conector and add cameras lesson

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import solidJs from "@astrojs/solid-js";
-
-// https://astro.build/config
-export default defineConfig({
-  output: "server",
-  integrations: [solidJs()]
-});
+export default defineConfig({});
