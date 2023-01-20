34178d45c0264665b564bb3aa5c02f3ee04d2d01
Victor Hugo González Hernández <64684570+Pikkirrin@users.noreply.github.com>
2023-01-20 01:23:54 -0600

    Add dist to github

---
+++ dist/hoisted.2bf2c567.js
@@ -0,0 +1,1 @@
+import{S as a,B as o,a as r,M as c,P as i,W as h}from"./chunks/three.module.6f927e5e.js";const s=new a,d=new o(1,1,1),w=new r({color:16711680}),m=new c(d,w);s.add(m);const e={width:600,height:400},t=new i(75,e.width/e.height);t.position.z=3;s.add(t);const g=document.querySelector(".webgl"),n=new h({canvas:g});n.setSize(e.width,e.height);n.render(s,t);
